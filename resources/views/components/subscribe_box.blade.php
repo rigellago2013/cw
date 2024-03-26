35fe52ac3fa0034da73fddc5cd440f1f675e7f95
Rigel Lago <rigellago2013@gmail.com>
2024-03-27 01:40:24 +0800

    Update

--- resources/views/components/subscribe_box.blade.php
+++ resources/views/components/subscribe_box.blade.php
@@ -4,7 +4,6 @@
     <!-- Begin Constant Contact Inline Form Code -->
 
        <div class="ctct-inline-form" data-form-id="bd7f616d-6346-418e-85ac-7e53f6c6c499">
-MURI TEST
        </div>
                                 <!-- End Constant Contact Inline Form Code -->
 </div>
